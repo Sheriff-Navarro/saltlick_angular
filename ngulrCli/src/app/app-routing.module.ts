a393ff19441c14a5cc59ec0b7098b16b8f4a3396
Nick Navarro <jacinto802@gmail.com>
2017-12-23 10:20:20 -0500

    Login, Logout, Signup all working via Angular

--- ngulrCli/src/app/app-routing.module.ts
+++ ngulrCli/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SignupComponent } from './signup/signup.component';
 import { RecipesComponent } from './recipes/recipes.component'
+import { LoginComponent } from './login/login.component';
+
 const routes: Routes = [
 
 
   {path: '', component: SignupComponent},
   {path: 'signup', component: SignupComponent  },
+  {path: 'login', component: LoginComponent },
   {path: 'recipes', component: RecipesComponent },
   {path: '**', redirectTo: ''}
 ];
--- ngulrCli/src/app/login/login.component.ts
+++ ngulrCli/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthServiceService } from '../services/auth-service.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginInfo = {
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  loginErrorMessage: string;
+
+  isLoggedOut: boolean = false;
+
+
+  constructor(
+    private authThang: AuthServiceService,
+    private routerThang: Router
+  ) { }
 
   ngOnInit() {
+    this.authThang.checklogin()
+      // If success, we are logged in.
+      .then((resultFromApi) => {
+          this.routerThang.navigate(['/recipes']);
+      })
+
+      // Even if you don't do anything on error, catch to avoid a console error.
+      .catch((err) => {
+          this.isLoggedOut = true;
+      });
   }
+
+  doLogin() {
+      this.authThang.login(this.loginInfo)
+        .then((resultFromApi) => {
+            // clear the form
+            this.loginInfo = {
+              email: '',
+              password: ''
+            };
+
+            // clear the error message
+            this.loginErrorMessage = "";
+
+            // redirect to /camels
+            this.routerThang.navigate(['/recipes']);
+        })
+        .catch((err) => {
+            const parsedError = err.json();
+            this.loginErrorMessage = parsedError.message + ' 😤';
+        });
+    } // close doLogin()
+
+
+
+
 
 }
