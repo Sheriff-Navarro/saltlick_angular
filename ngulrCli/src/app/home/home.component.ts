847abb1181c6511ae5d3202628aa5aa6432c0aa8
Nick Navarro <jacinto802@gmail.com>
2017-12-30 13:29:27 -0500

    Home Component ==> Login & Signup working

--- ngulrCli/src/app/home/home.component.ts
+++ ngulrCli/src/app/home/home.component.ts
@@ -1,15 +1,106 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from '../../environments/environment';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthServiceService } from '../services/auth-service.service';
+import { RecipeServiceService } from '../services/recipe-service.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [AuthServiceService, RecipeServiceService]
 })
 export class HomeComponent implements OnInit {
+  isShowingLogin: boolean = false;
+  isShowingSignup: boolean = false;
+  isLoggedOut: boolean = false;
 
-  constructor() { }
 
-  ngOnInit() {
+  loginInfo = {
+    email: '',
+    password: ''
+  };
+
+  loginErrorMessage: string;
+
+  signUpInfo = {
+    fullName: '',
+    email: '',
+    password: ''
+  };
+
+  errorMessage: string;
+
+
+  constructor(
+  private routerThang: Router,
+  private route: ActivatedRoute,
+  private authThang: AuthServiceService,
+  private recipeThang: RecipeServiceService,
+) { }
+
+  ngOnInit(
+    ) {
   }
+
+  showLoginForm() {
+    this.isShowingLogin = true;
+  }//close showRecipeForm();
+
+
+  showSignupForm() {
+      this.isShowingSignup = true;
+    }//close showRecipeForm();
+
+
+    doSignUp() {
+       this.authThang.signup(this.signUpInfo)
+         .then((resultFromApi) => {
+             // clear form
+             this.signUpInfo = {
+               fullName: '',
+               email: '',
+               password: ''
+             };
+
+             // clear error message
+             this.errorMessage = "";
+
+             // redirect to /camels
+             this.routerThang.navigate(['/signup']);
+         })
+         .catch((err) => {
+             const parsedError = err.json();
+             this.errorMessage = parsedError.message + ' 😤';
+         });
+     } // close doSignUp()
+
+     doLogin() {
+         this.authThang.login(this.loginInfo)
+           .then((resultFromApi) => {
+               // clear the form
+               this.loginInfo = {
+                 email: '',
+                 password: ''
+               };
+
+               // clear the error message
+               this.loginErrorMessage = "";
+
+               // redirect to /camels
+               this.routerThang.navigate(['/recipes']);
+           })
+           .catch((err) => {
+               const parsedError = err.json();
+               this.loginErrorMessage = parsedError.message + ' 😤';
+           });
+       } // close doLogin()
+
+
+
+
+
+
 
 }
