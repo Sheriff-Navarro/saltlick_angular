31df71eb9b2885a452ac07a900a6a5a8d7fabe74
Nick Navarro <jacinto802@gmail.com>
2018-01-11 18:07:51 -0500

    Profile page

--- ngulrCli/src/app/profile/profile.component.ts
+++ ngulrCli/src/app/profile/profile.component.ts
@@ -20,7 +20,27 @@ export class ProfileComponent implements OnInit {
   userRecipeArray: any[] = [];
   recipeListError: string;
   baseUrl = environment.apiBase;
+  data: any;
 
+  isShowingForm: boolean = false;
+  isShowingSearch: boolean = true;
+
+
+  recipeInfo = {
+    recipeName: "",
+    recipeCookTime: undefined,
+    recipeServes: undefined,
+    recipeIngredients: [],
+    recipeDirections: [],
+    recipeType: ""
+  }
+
+  myCoolUploader = new FileUploader({
+   url: environment.apiBase + '/api/recipes',
+   itemAlias: 'recipePicture'
+ });
+
+ saveError: string;
 
   constructor(
     private routerThang: Router,
@@ -42,15 +62,105 @@ export class ProfileComponent implements OnInit {
       });
   }
 
+  // getThemProfileRecipe(id) {
+  //   this.profileThang.getProfile(id)
+  //   .subscribe(
+  //     (usersRecipes) => { this.userRecipeArray = usersRecipes },
+  //     () => {
+  //       this.recipeListError = 'Sorry, could not retrieve all the recipes'
+  //     }
+  //   );
+  // }//close getThemRecipes.
+
   getThemProfileRecipe(id) {
     this.profileThang.getProfile(id)
     .subscribe(
-      (usersRecipes) => { this.userRecipeArray = usersRecipes },
+      (data) => { this.data = data },
       () => {
         this.recipeListError = 'Sorry, could not retrieve all the recipes'
       }
     );
   }//close getThemRecipes.
+
+  saveNewRecipe() {
+    //if no recipe, regular AJAX upload
+    if (this.myCoolUploader.getNotUploadedItems().length===0){
+    this.saveRecipeNoPicture();
+  }
+  //else, upload pictures with FileUploader
+  else {
+  this.saveRecipewithPicture();
+  }
+  } //close saveNewRecipe ()
+
+  addIngredient(ingredient) {
+    this.recipeInfo.recipeIngredients.push(ingredient);
+  }
+
+  removeIngredient(ingredient){
+  this.recipeInfo.recipeIngredients.pop();
+  }
+
+  addDirection(direction) {
+    this.recipeInfo.recipeDirections.push(direction);
+  }
+
+  removeDirection(ingredient) {
+  this.recipeInfo.recipeDirections.pop();
+  }
+
+  private saveRecipeNoPicture() {
+  this.recipeThang.newRecipe(this.recipeInfo)
+  .subscribe(
+    (newRecipeFromApi) => {
+      this.data.recipe.push(newRecipeFromApi);
+      this.isShowingForm = false;
+      this.recipeInfo = {
+        recipeName: "",
+        recipeCookTime: undefined,
+        recipeServes: undefined,
+        recipeIngredients: [],
+        recipeDirections: [],
+        recipeType: ""
+      };
+      this.saveError = 'There was an error saving the recipe.'
+    }
+  );
+  }//close saveRecipeNoPicture
+
+  private saveRecipewithPicture() {
+  this.myCoolUploader.onBuildItemForm = (item, form) => {
+    form.append('recipeName', this.recipeInfo.recipeName);
+    form.append('recipeServes', this.recipeInfo.recipeServes);
+    form.append('recipeCookTime', this.recipeInfo.recipeCookTime);
+    form.append('recipeType', this.recipeInfo.recipeType);
+    form.append('recipeIngredients', JSON.stringify(this.recipeInfo.recipeIngredients));
+    form.append('recipeDirections', JSON.stringify(this.recipeInfo.recipeDirections));
+
+  };
+  this.myCoolUploader.onSuccessItem = (item, response) =>{
+    console.log(item);
+    const newRecipeFromApi = JSON.parse(response);
+    this.data.recipe.push(newRecipeFromApi);
+    this.isShowingForm = false;
+    this.recipeInfo = {
+      recipeName: "",
+      recipeCookTime: undefined,
+      recipeServes: undefined,
+      recipeIngredients: [],
+      recipeDirections: [],
+      recipeType: ""
+    };
+    this.saveError = '';
+  };
+
+  this.myCoolUploader.onErrorItem = (item, response) => {
+    console.log(item, response);
+    this.saveError = 'New Recipe could not be saved with picture.'
+  }
+  //this is the function that initiates the AJAX request
+  this.myCoolUploader.uploadAll();
+  }//closes saveRecipewithPicture
 
 
 }
